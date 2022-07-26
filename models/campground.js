ff2059532cc7d1f254a3cb1be274d28630349c7b
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-07-26 23:07:31 +0200

    module not found fix

--- models/campground.js
+++ models/campground.js
@@ -5,7 +5,6 @@ const CampgroundSchema = new Schema({
   title: String,
   price: String,
   description: String,
-  location: String,
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
