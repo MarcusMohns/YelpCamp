ff9512ee270312ac2ada817eb4c83320bda9b9d6
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-08-18 01:50:24 +0200

    flash success partial

--- routes/reviews.js
+++ routes/reviews.js
@@ -28,6 +28,7 @@ router.post(
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash("success", "Created new review!");
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
@@ -39,6 +40,7 @@ router.delete(
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
     const campground = await Campground.findById(id);
+    req.flash("success", "Successfully deleted review!");
     res.redirect(`/campgrounds/${id}`);
   })
 );
