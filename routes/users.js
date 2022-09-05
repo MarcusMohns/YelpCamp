9ed4d5926a11dbaf2cc95dafe4556607f4bd1ed6
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-09-05 18:37:34 +0200

    Fixing register route

--- routes/users.js
+++ routes/users.js
@@ -15,13 +15,16 @@ router.post(
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       const registeredUser = await User.register(user, password);
+
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "Welcome to Yelp Camp");
+        res.redirect("/campgrounds");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("register");
     }
-    console.log(registeredUser);
-    req.flash("success", "Welcome to Yelp Camp");
-    res.redirect("/campgrounds");
   })
 );
 
