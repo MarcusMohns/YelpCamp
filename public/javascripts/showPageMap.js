15828b9757a5b0583b705af6a471ed774ddc49f0
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-09-29 14:13:26 +0200

    added backup coordinates if no place is found

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,8 +1,8 @@
 mapboxgl.accessToken = mapToken;
-const map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v11", // style URL
-  center: campground.geometry.coordinates, // starting position [lng, lat]
+  center: campground.geometry.coordinates || [12.550343, 55.665967], // starting position [lng, lat]
   zoom: 9, // starting zoom
   projection: "globe", // display the map as a 3D globe
 });
@@ -10,4 +10,6 @@ map.on("style.load", () => {
   map.setFog({}); // Set the default atmosphere style
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates || [12.550343, 55.665967])
+  .addTo(map);
