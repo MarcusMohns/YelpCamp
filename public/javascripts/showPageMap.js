fab01281a0df1193797d5bc00f30e29875755e51
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-09-30 23:55:48 +0200

    changed theme to light

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 let map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  style: "mapbox://styles/mapbox/light-v10", // style URL
   center: campground.geometry.coordinates || [12.550343, 55.665967], // starting position [lng, lat]
   zoom: 9, // starting zoom
   projection: "globe", // display the map as a 3D globe
