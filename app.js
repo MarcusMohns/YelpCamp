b8ad2cd6028c486d2c61a9dbd8e406dccc0333f2
MarcusMohns <92151780+MarcusMohns@users.noreply.github.com>
2022-11-28 21:15:18 -0200

    ready to try deploying

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const MongoDBStore = require("connect-mongo")(session);
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
-const dbURL = "mongodb://localhost:27017/yelp-camp";
+const dbURL = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
 mongoose.connect(dbURL, {
   useNewUrlParser: true,
@@ -47,9 +47,11 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 
+const secret = process.env.SECRET || "thisshouldbeabettersecret";
+
 const store = new MongoDBStore({
   url: dbURL,
-  secret: "thisshouldbeabettersecret",
+  secret,
   touchAfter: 24 * 60 * 60,
 });
 
@@ -60,7 +62,7 @@ store.on("error", function (e) {
 const sessionConfig = {
   store,
   name: "session",
-  secret: "thisshouldbeabettersecret!",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
