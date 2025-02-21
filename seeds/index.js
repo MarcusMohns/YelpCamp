40778bea955a56c93b6031e20e4c6c65cae9f1b3
Marcus Mohns <marcus@mohns.se>
2025-02-21 16:10:17 +0100

    fix show page

--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,7 @@ const seedDB = async () => {
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       //YOUR USER ID
-      author: "5f5c330c2cd79d538f2c66d9",
+      author: "67b896e9a45cf82e6c8ef0d8",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       description:
