4668ccea4d36f169cd2a7ee705139ea21fa8fb07
Marcus Mohns <marcus@mohns.se>
2025-02-21 16:35:53 +0100

    add in cloudinary again

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use(
         "'self'",
         "blob:",
         "data:",
-        "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+        "https://res.cloudinary.com/dzc6mwzrx/",
         "https://images.unsplash.com/",
         "https://api.maptiler.com/",
       ],
