530c5d16460c37d765f6e50f12ce6214e76a95dc
Marcus Mohns <marcus@mohns.se>
2025-02-25 13:48:19 +0100

    add default loc Sweden and add swedish campgrounds

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,8 +2,8 @@ maptilersdk.config.apiKey = maptilerApiKey;
 
 const map = new maptilersdk.Map({
   container: "cluster-map",
-  style: maptilersdk.MapStyle.BRIGHT,
-  center: [-103.59179687498357, 40.66995747013945],
+  style: maptilersdk.MapStyle.STREETS,
+  center: [15.011743, 62.944252],
   zoom: 3,
 });
 
