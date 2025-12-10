b668c03d54e5ec1adf7d29b9400646754d9ab5ed
Marcus Mohns <marcus@mohns.se>
2025-12-10 19:58:26 +0100

    place favicon in served public folder

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ db.once("open", () => {
 });
 
 const app = express();
-app.use(favicon(__dirname + "/favicon/favicon.ico"));
+app.use(favicon(__dirname + "/public/favicon/favicon.ico"));
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
